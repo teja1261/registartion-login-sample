de21a2916213da73da067af3b21cc0bc30a1b59d
teja1261 <teja1261narendra@gmail.com>
2022-07-26 22:42:04 +0530

    Fetched data  console

--- src/Component/HomePage/HomePage.js
+++ src/Component/HomePage/HomePage.js
@@ -1,19 +1,63 @@
 import React, { useState, useEffect } from "react";
 import Header from "../Header/Header";
+import {fetchProductsAPI} from "../../Services/ProductsAPI";
+import { RotatingLines } from "react-loader-spinner";
 
 const HomePage = () => {
-    const dataApiCall = () => {
-        
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [loader, setLoader] = useState(false);
+
+
+  const dataApiCall = async(latitude, longitude) => {
+      setLoader(true);
+      const fetchRequest = await fetchProductsAPI(latitude, longitude);
+      if(fetchRequest.ok){
+        const dataRes = await fetchRequest.json();
+        setLoader(false);
+        console.log(dataRes.data.products);
+      }else{
+        setLoader(false);
+        console.log(fetchRequest)
+      }
+
+  }
+  useEffect(() => {
+
+    if (!navigator.geolocation) {
+      setStatus('Geolocation is not supported by your browser');
+    } else {
+      setStatus('Locating...');
+      navigator.geolocation.getCurrentPosition((position) => {
+        setStatus(null);
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, () => {
+        setStatus('Unable to retrieve your location');
+      });
     }
-  useEffect(() => {}, []);
+
+    dataApiCall(lat,lng)
+  }, []);
 
   return (
-    <div>
+    <>
       <Header />
-      <div>
-        <h1>Hello</h1>
-      </div>
-    </div>
+      {loader === true ? <div className="home-main-container">
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="96"
+            visible={true}
+          />
+        </div>: <div className="home-main-container">
+        <h1>{status}</h1>
+        <h2>latitude : {lat}</h2>
+        <h2>longitude : {lng}</h2>
+      </div>}
+    </>
   );
 };
 
--- src/Services/ProductsAPI.js
+++ src/Services/ProductsAPI.js
@@ -1,6 +1,8 @@
-export const emailSendingOTPAPI = async (email) => {
+import Cookies from 'js-cookie';
+
+export const fetchProductsAPI = async (latitude, longitude) => {
   const jwtToken = Cookies.get("jwt_token");
-  const url = "https://ring-ring-food.herokuapp.com/api/user/send-otp-email";
+  const url = "https://ring-ring-food.herokuapp.com/api/product/get-list";
   const options = {
     headers: {
       "content-type": "application/json",
@@ -9,7 +11,13 @@ export const emailSendingOTPAPI = async (email) => {
     },
     method: "POST",
     body: JSON.stringify({
-      email,
+      latitude,
+      longitude,
+      "radius": 2,
+      "sorts": {
+        "rating": -1
+      },
+      "limit": 10
     }),
   };
 
